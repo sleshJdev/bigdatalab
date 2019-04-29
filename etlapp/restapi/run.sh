10dfd1474c5679d8de64d3ed26a4ac646b4bea1c
Eugene <slesh.eugene93@gmail.com>
2019-04-29 06:19:37 +0300

    added redis

--- etlapp/restapi/run.sh
+++ etlapp/restapi/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sbt dist
-docker build -t bigdatalab:restapi .
-docker run --rm -ti --name dev_bigdatalabrestapi -p 9000:9000 -p 9902:9902 -p 8802:8802 bigdatalab:restapi
+cp -f ./target/universal/restapi-1.0-SNAPSHOT.zip ./docker/restapi/
+docker-compose up --build -d
