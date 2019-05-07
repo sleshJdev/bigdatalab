4d09fda431a1dee401ae376b6d72ab563c849852
Eugene <slesh.eugene93@gmail.com>
2019-05-07 15:24:47 +0300

    updated gatling script

--- bitoolsapp/gatling.sh
+++ bitoolsapp/gatling.sh
@@ -4,6 +4,6 @@ while :; do
     # count - number order to be generated, e.g. count=3
     # exception - exception message to be logged, e.g. &exception=OrderError
     # latency - response latency in seconds, e.g. &latency=3
-    curl "http://localhost:9889/v1/orders/generate?count=3Error&latency=1";
+    curl "http://localhost:9889/v1/orders/generate?count=3&exception=OrderError&latency=2";
     sleep 2s;
 done
