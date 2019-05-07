17f94364af4cccf05d2fd9e0d7014ae45ef343d2
Eugene <slesh.eugene93@gmail.com>
2019-05-07 12:13:58 +0300

    fixed gatling curl

--- bitoolsapp/gatling.sh
+++ bitoolsapp/gatling.sh
@@ -2,5 +2,5 @@
 
 while :; do
     sleep 10s;
-    curl "http://localhost:9889/v1/orders/generate&exception=OrderError&latency=3";
+    curl "http://localhost:9889/v1/orders/generate?exception=OrderError&latency=3";
 done
