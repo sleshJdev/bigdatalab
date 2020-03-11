c33a768a18bdd031787d82053ac25dccc0f22212
Eugene <slesh.eugene93@gmail.com>
2020-03-11 08:08:40 +0300

    updated logback pipelines

--- bitoolsapp/gatling.sh
+++ bitoolsapp/gatling.sh
@@ -5,5 +5,5 @@ while :; do
     # exception - exception message to be logged, e.g. &exception=OrderError
     # latency - response latency in seconds, e.g. &latency=3
     curl "http://localhost:9889/v1/orders/generate?count=3&exception=OrderError&latency=2";
-    sleep 2s;
+    sleep 5s;
 done
